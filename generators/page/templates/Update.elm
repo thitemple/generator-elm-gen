5d234538d4affdd33b08a5eeddf941bcb4c91999
Thiago Temple <vintem@gmail.com>
2018-09-19 10:46:45 -0400

    Fixes update template

--- generators/page/templates/Update.elm
+++ generators/page/templates/Update.elm
@@ -11,16 +11,20 @@ import Pages.<%= module %>.Model exposing (Model)
 import RemoteData exposing (RemoteData(..))
 import Routing.Route as Route exposing (VerificationStep)
 import Routing.Router exposing (VerificationResult(..))
-import Types exposing (SessionInfo)
+import Types
+    exposing
+        ( AppConfigurations
+        , SessionInfo
+        )
 
 
-onArrival : Maybe SessionInfo -> VerificationStep RootModel.Model RootMsg.Msg
-onArrival sessionInfo { model } =
+onArrival : AppConfigurations -> SessionInfo -> VerificationStep RootModel.Model RootMsg.Msg
+onArrival configs sessionInfo { model } =
     Allowed ((model , Cmd.batch []))
 
 
-update : Maybe SessionInfo -> Msg -> Model -> ( Model, Cmd RootMsg.Msg )
-update sessionInfo msg model =
+update : AppConfigurations -> SessionInfo -> Msg -> Model -> ( Model, Cmd RootMsg.Msg )
+update configs sessionInfo msg model =
     case msg of
         _ ->
             (model , Cmd.none)
